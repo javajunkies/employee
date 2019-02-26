92a8d79c060c4cdd22c76e40ad620fe99a208ad6
DKUHR001 <dkuhr001@vplinphys02.ad.csbsju.edu>
2019-02-25 19:01:09 -0600

    Terminal 1 in Step 5: Modified Employee.java introducing a conflict

--- Driver.java
+++ Driver.java
@@ -14,7 +14,8 @@ public class Driver
       harry.raiseSalary(10);
       // print out information about harry
       // use java.lang.System.out here
-      out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
+      //out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
+      out.println(harry);
    }
 }
 
